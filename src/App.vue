fec5781819ac5a203233ce2189bf6fb5da22ad65
LeoDanSG <sleod239@hotmail.com>
2024-02-29 12:50:32 -0600

    ⭐Feat: App.vue Add a header within the UI

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-
+//Importando funcion para crear referencias reactivas
+import { ref } from 'vue'
+//Creando una referencia de tipo string
+const header = ref('App Lista de compras');
+const shoppingIcon = ref('material-icons shopping-cart-icon');
 </script>
 
 <template>
-  <h1>APP</h1>
+  <h1> <i v-bind:class="shoppingIcon">local_mall</i> {{ header }}</h1>
 </template>
 
 <style scoped>
+.shopping-cart-icon {
+  font-size: 2rem;
+}
 </style>
