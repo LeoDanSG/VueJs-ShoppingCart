2db3840d23f0b43770111ad38c72fa5eddc53ade
LeoDanSG <sleod239@hotmail.com>
2024-03-14 13:10:18 -0600

    🎯 S02E01 Conditional Rendering (Boton para mostrar el formulario)

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,15 @@ const header = ref('App Lista de compras');
 const shoppingIcon = ref('material-icons shopping-cart-icon');
 //Creando un referencia reactiva para almacenar el valor de la lista
 const items = ref([
-  {id: 0, label:'Leche'},
-  {id: 1, label:'Arroz'},
-  {id: 2, label:'Carne'},
-  {id: 3, label:'Pan'},
-  {id: 4, label:'Galletas'}
+//{id: 0, label:'Leche'},
+//{id: 1, label:'Arroz'},
+//{id: 2, label:'Carne'},
+//{id: 3, label:'Pan'},
+// {id: 4, label:'Galletas'}
 ]);
 const newItem = ref('');
 const newItemHighPriority = ref (false)
+
 //Metodos
 const saveItems = () => {
   //Agrega un nuevo elemento a la lista proveniente de la caja de texto 
@@ -21,25 +22,35 @@ const saveItems = () => {
   //Borramos el contenido de la caja de texto
   newItem.value = "";
 };
+const noItemsMessage = ref(true);
+const showForm = ref(false);
+const cancelForm = () => {
+  showForm.value = !showForm.value;
+}; 
 </script>
 
-
 <template>
-  <h1> <i v-bind:class="shoppingIcon">local_mall</i> {{ header }}</h1>
-  
-  <form v-on:submit.prevent="saveItems" class="add-item form">
+  <div class="header">
+  <h1> 
+    <i v-bind:class="shoppingIcon">local_mall</i> {{ header }}
+  </h1>
+  <button class="btn btn-primary" @click="cancelForm">{{ showForm ? 'Cancelar' : 'Agregar Articulo' }}</button>
+</div>
+  <form v-if="showForm" v-on:submit.prevent="saveItems" class="add-item form">
     
     <input v-model="newItem" type="text" placeholder="Agregar Articulo">
+  
     
     <label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label>
-    
     <button class="btn btn-primary">
     Agrear Articulo</button>
     </form>
  
   <ul>
+
     <li v-for="({id, label}, i) in items" v-bind:key="id">⭐{{ label }}</li>
   </ul>
+ <p v-if="items.length === 0">🥀No hay elementos en la lista🥀</p> 
 </template>
 
 <style scoped>
